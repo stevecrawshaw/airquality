e4f5baa0c56681903b144dd84b262b8b89e7c21a
Steve Crawshaw <steve.crawshaw@bristol.gov.uk>
2020-05-20 12:11:15 +0100

    update for OD colleauges

--- gg_themes.R
+++ gg_themes.R
@@ -3,13 +3,13 @@ has   <- wants %in% rownames(installed.packages())
 if(any(!has)) install.packages(wants[!has])
 lapply(wants, library, character.only=T)
 
-# Sys.setenv(TZ = "Etc/GMT-0")
+Sys.setenv(TZ = "Etc/GMT-0")
 # dateFrom = "2020-02-01"
 # dateTo = "2020-02-29"
 # startdate <- as.Date(dateFrom)
 # enddate <- as.Date(dateTo)
 
-# source("S:\\SUSTAIN\\EnvQual\\Air_Quality\\Projects\\R Projects\\ODS\\importODS.r")
+# source("S:\\SUSTAIN\\EnvQual\\Air_Quality\\Projects\\R Projects\\airquality_GIT\\importODS.r")
 # #download data
 # aq_data_DT_ggtheme <- importODSAQ(
 #     siteid = "all",
@@ -17,17 +17,17 @@ lapply(wants, library, character.only=T)
 #     dateFrom = dateFrom,
 #     dateTo = dateTo
 # )
-# 
-# # saveRDS(aq_data_DT_ggtheme, file = here::here("data", "aq_data_DT_ggtheme.rds"))
+# # 
+# saveRDS(aq_data_DT_ggtheme, file = here::here("data", "aq_data_DT_ggtheme.rds"))
 # 
 # 
 # aq_data_DT_ggtheme <- readRDS(here::here("data", "aq_data_DT_ggtheme.rds"))
-# #generate plot data for testing
-# plot_DT <- aq_data_DT_ggtheme %>% 
-#     select(-siteid) %>% 
-#     timeAverage(avg.time = "day", type = "location") %>% 
-#     rename(Date = date, Location = location) %>% 
-#     pivot_longer(cols = no2:no, names_to = "Pollutant", values_to = "Concentration") %>% 
+#generate plot data for testing
+# plot_DT <- aq_data_DT_ggtheme %>%
+#     select(-siteid) %>%
+#     timeAverage(avg.time = "day", type = "location") %>%
+#     rename(Date = date, Location = location) %>%
+#     pivot_longer(cols = no2:no, names_to = "Pollutant", values_to = "Concentration") %>%
 #     mutate(Pollutant = as.factor(toupper(Pollutant)))
 # 
 # glimpse(plot_DT)
@@ -65,14 +65,14 @@ theme_report_facet <- function () {
 theme_ppt_single <- function(){
     theme_bw(base_size = 14) %+replace%
         theme(
-            plot.title = element_text(size = 20, face = "bold", margin = margin(0,0,15,0)),
-            plot.subtitle = element_text(size = 15, face = "bold", margin = margin(10,0,10,0)),
+            plot.title = element_text(size = 18, face = "bold", margin = margin(0,0,15,0), hjust = 0.5),
+            plot.subtitle = element_text(size = 12, face = "bold", margin = margin(10,0,10,0)),
             panel.grid.major = element_line(color = "darkgray"),
-            axis.text.x = element_text(size = 20, face = "bold"),
-            axis.text.y = element_text(size = 20, face = "bold"),
-            axis.title.y = element_text(size = 20, face = "bold", angle = 90),
-            axis.title.x = element_text(size = 20, face = "bold"),
-            legend.text = element_text(size = 12),
+            axis.text.x = element_text(size = 18, face = "bold"),
+            axis.text.y = element_text(size = 18, face = "bold"),
+            axis.title.y = element_text(size = 18, face = "bold", angle = 90),
+            axis.title.x = element_text(size = 18, face = "bold"),
+            legend.text = element_text(size = 18),
             strip.text.y = element_text(color = "black", face = "bold")
         )
     
